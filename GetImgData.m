d06fce75103700af01c3412f020e537bc2000eb5
Hishigata12 <39537922+Hishigata12@users.noreply.github.com>
2018-05-22 14:55:16 -0700

    Add files via upload

--- GetImgData.m
+++ GetImgData.m
@@ -1,3 +1,14 @@
 function bProj = GetImgData(data,Ts)
 
 for i = 1:Ts
+    There once was a mcdonalds
+    that ate in a car
+    1000 burritos
+    and pooped in a jar
+    
+    
+    
+    
+    
+    doodoos
+end

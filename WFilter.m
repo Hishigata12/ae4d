840bf432313972a26923ba0e7fefbf18a1b3a651
Hishigata12 <39537922+Hishigata12@users.noreply.github.com>
2018-06-12 14:54:48 -0700

    Delete WFilter.m

--- WFilter.m
+++
@@ -1,9 +0,0 @@
-% Creates a weiner filter for reducing noise
-% Generally not useful, but might be helpful for removing noise in pulse
-% data
-% R asks for time axis type
-% ln is line of M-Mode data to acquire noise profile
-
-function [X] = WFilter(LF,TX)
-
-if R == 'ae' || R == 'pe' || R == 'f' || R == 'fast' || R == 'M'

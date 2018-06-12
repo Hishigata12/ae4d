77208547c89a5a2ab66217a555f6f39104adaccb
Hishigata12 <39537922+Hishigata12@users.noreply.github.com>
2018-06-12 14:56:38 -0700

    Delete testcode.m

--- testcode.m
+++
@@ -1,7 +0,0 @@
-function [daq velmex hp] = testcode(b);
-%This must exist at the beginning of all codes
-global floc
-[param HF LF] = read_ucsdi_data(floc,b);
-velmex = param.velmex;
-hp = param.hp;
-daq = param.daq;
